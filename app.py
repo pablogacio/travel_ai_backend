fd9e177f24fabd287c8078cad525fd31a4904547
Pablo <pablogacio3@gmail.com>
2026-01-14 15:51:52 +0100

    Actualizar app.py para producción

--- app.py
+++ app.py
@@ -35,5 +35,5 @@ def generar_itinerario():
 
 if __name__ == "__main__":
     import os
-    port = int(os.environ.get("PORT", 5000))  # Railway asigna el puerto automáticamente
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
